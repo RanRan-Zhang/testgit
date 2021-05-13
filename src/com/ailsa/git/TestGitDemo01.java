2814813899947a3694e992b68c63485c206cf29a
ranran01 <969634214@qq.com>
2021-05-13 22:57:21 +0800

    提交个fenzhi1

--- src/com/ailsa/git/TestGitDemo01.java
+++ src/com/ailsa/git/TestGitDemo01.java
@@ -1,8 +1,10 @@
 package com.ailsa.git;
-
 public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这是TestGitDemo01");
         System.out.println("我修改了TestGitDemo01");
+    }
+    public void testAdd(){
+        System.out.println("测试分支方法");
     }
 }
